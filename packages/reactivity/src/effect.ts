e7ad9a00717451519b57c05ea823cfbf2b0ebc0a
coderjc <coderjc@qq.com>
2025-08-01 02:01:05 +0800

    refactor: 优化实现解决嵌套effect的方案

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -8,20 +8,16 @@ export class ReactiveEffect {
         this.fn = fn
     }
     run() {
-        activeSub = this
+        // 在将当前的 effect 赋值给 activeSub 之前，先将前一个 activeSub 保存起来
+        const prev = activeSub
 
-        // 为了解决effect嵌套问题，需要将effect放入栈中
-        effectStack.push(this)
+        activeSub = this
 
         try {
             return this.fn()
         } finally {
-            // 执行完成之后，删除当前effect
-            effectStack.pop()
-            // 删除完成之后，如果存在effectStack，则将activeSub指向最后一个effect，不存在则指向undefined
-            activeSub = effectStack.length
-                ? effectStack[effectStack.length - 1]
-                : undefined
+            // 恢复之前的 activeSub
+            activeSub = prev
         }
     }
 }
