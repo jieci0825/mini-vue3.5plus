e0c13f7495d28a63290ce38c41b13dbb6d88bb88
coderjc <coderjc@qq.com>
2025-08-02 17:12:46 +0800

    fix: 解决依赖重复的问题

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,12 +1,22 @@
+import { Link } from './system'
+
 export let activeSub: ReactiveEffect | undefined = undefined
 
 export class ReactiveEffect {
     fn: Function
+    deps: Link | undefined
+    depsTail: Link | undefined
     constructor(fn: Function) {
         this.fn = fn
     }
 
     run() {
+        // 为了后续可以复用，而不创建新的 link 节点，所以每次执行前都把 depsTail 置空为 undefined
+        //  - 起到的是一个标记的作用
+        //  - 即如果 deps 和 depsTail 都为 undefined，则说明当前 effect 没有依赖，是第一次执行
+        //  - 如果 deps 存在但是 depsTail 为 undefined，则说明不是第一次执行了
+        this.depsTail = undefined
+
         // 在将当前的 effect 赋值给 activeSub 之前，先将前一个 activeSub 保存起来
         const prev = activeSub
 
@@ -34,12 +44,7 @@ type EffectFunction = {
     effect?: ReactiveEffect
 }
 
-export function effect(
-    fn: Function,
-    options: any = {
-        scheduler: undefined
-    }
-) {
+export function effect(fn: Function, options: any = {}) {
     const _effect = new ReactiveEffect(fn)
 
     Object.assign(_effect, options)
--- packages/reactivity/src/system.ts
+++ packages/reactivity/src/system.ts
@@ -1,11 +1,30 @@
 import type { ReactiveEffect } from './effect'
 import type { RefImpl } from './ref'
 
+// 依赖实例
+//  - 即当前 ref/reactive/... 的订阅者是哪个 effect
+export interface Dep {
+    // 订阅者链表的头结点
+    subs: Link | undefined
+    // 订阅者链接的尾节点
+    subsTail: Link | undefined
+}
+
+// 订阅者实例
+//  -  即当前 effect 的依赖项是哪个 ref/reactive/...
+export interface Sub {
+    deps: Link | undefined
+    depsTail: Link | undefined
+}
+
 export interface Link {
-    // 保存 effect
-    sub: ReactiveEffect
+    // 订阅者(即effect)-双向
+    sub: Sub
     nextSub: Link | undefined
     prevSub: Link | undefined
+    // 依赖实例(如ref reactive)-单向
+    dep: Dep
+    nextDep: Dep | undefined
 }
 
 /**
@@ -14,12 +33,36 @@ export interface Link {
  * @param sub effect 函数
  */
 export function link(dep: RefImpl, sub: ReactiveEffect) {
+    /* // 在此处进行检测，如果 dep(ref/reactive...) 已经和 sub(effect) 存在关联关系了，则不在创建新的 link
+    const currentDeps = sub.depsTail
+    // 如果当前 sub(effect) 的尾部依赖为空，且头部依赖存在，则表示当前 sub(effect) 已经和 dep(ref/reactive...) 存在关联关系了
+    // Tips: 在 effect 的执行 fn 前，会将 sub.depsTail 置为空，来作为标志
+    if (currentDeps === undefined && sub.deps) {
+        // 检测当前 sub(effect) 的头部依赖是否为 dep(ref/reactive...)
+        if (sub.deps.dep === dep) {
+            // 如果存在，则直接返回，且恢复 sub.depsTail 的值
+            sub.depsTail = sub.deps
+            return
+        }
+    } */
+
+    const nextDep = sub.depsTail === undefined ? sub.deps : sub.depsTail.nextDep
+    // @ts-ignore
+    if (nextDep && nextDep.dep === dep) {
+        // @ts-ignore
+        sub.depsTail = nextDep
+        return
+    }
+
     // 构建一个新的链接节点
     const newLink: Link = {
         sub,
         nextSub: undefined,
-        prevSub: undefined
+        prevSub: undefined,
+        dep: dep,
+        nextDep: undefined
     }
+
     // 检测是否存在尾节点
     if (!dep.subsTail) {
         // 1. 不存在在则加入头结点，且头尾节点相同
@@ -32,6 +75,16 @@ export function link(dep: RefImpl, sub: ReactiveEffect) {
         newLink.prevSub = dep.subsTail
         // 并且将当前新节点作为ref中的新的尾节点
         dep.subsTail = newLink
+    }
+
+    // 如果 sub(effect) 的头部和尾部依赖都是空，则表示是第一次需要建立关联关系
+    if (!sub.depsTail) {
+        sub.deps = newLink
+        sub.depsTail = newLink
+    } else {
+        // @ts-ignore
+        sub.depsTail.nextDep = newLink
+        sub.depsTail = newLink
     }
 }
 
@@ -46,7 +99,7 @@ export function propagate(dep: RefImpl) {
     const queueEffect: ReactiveEffect[] = []
     // 遍历订阅者-直到最后一个尾节点
     while (link) {
-        queueEffect.push(link.sub)
+        queueEffect.push(link.sub as ReactiveEffect)
         // 将当前节点指向下一个节点
         link = link.nextSub
     }
